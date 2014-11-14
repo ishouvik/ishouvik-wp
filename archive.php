176fe8f9072ca9f775a2c752ba53e7ad14025a90
Shouvik Mukherjee <contact@ishouvik.com>
2014-11-14 15:50:05 +0530

    Bugfixes

---
+++ archive.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Description: Default Archive template to display loop of blog posts
+ *
+ * @package WordPress
+ * @subpackage iShouvik WP
+ */
+get_header(); ?>
+<div class="container">
+	<div class="clearfix">
+	    <div class="col-md-8">
+	        <?php if (have_posts()): ?>
+	        	<header class="well">
+	        		<h1 class="archive-title">
+	        			<?php
+							if ( is_day() ) :
+								printf( __( 'Daily Archives: %s', 'ishouvikwp' ), get_the_date() );
+							elseif ( is_month() ) :
+								printf( __( 'Monthly Archives: %s', 'ishouvikwp' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'ishouvikwp' ) ) );
+							elseif ( is_year() ) :
+								printf( __( 'Yearly Archives: %s', 'ishouvikwp' ), get_the_date( _x( 'Y', 'yearly archives date format', 'ishouvikwp' ) ) );
+							else :
+								_e( 'Archives', 'ishouvikwp' );
+							endif;
+						?>
+	        		</h1>
+	        	</header>
+	        	<?php while (have_posts()) : the_post(); ?>
+		            <?php get_template_part( 'content', get_post_format() ); ?>
+		        <?php endwhile; ?>
+	        <?php endif; ?>
+	    </div>
+	    <div class="col-md-4 hidden-xs">
+	        <?php get_sidebar('blog'); ?>
+	    </div>
+	</div>
+</div>
+
+<?php ishouvik_pagination(); ?>
+
+<?php get_footer(); ?>
--- category.php
+++ category.php
@@ -13,6 +13,9 @@ get_header(); ?>
 	    	<?php if (have_posts()) :  ?>
 	    		<header class="well">
 	    			<h1 class="archive-title"><?php printf( __( 'Category: %s', 'ishouvikwp' ), single_cat_title( '', false ) ); ?></h1>
+	    			<?php if ( category_description() ) : // Show an optional category description ?>
+	    				<div class="archive-meta"><?php echo category_description(); ?></div>
+	    			<?php endif; ?>
 	    		</header>
 		        <?php while (have_posts()) : the_post(); ?>
 		            <?php get_template_part( 'content', get_post_format() ); ?>
